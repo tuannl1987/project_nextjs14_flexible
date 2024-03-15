b1edf5f0d98e70dd52242392ae534afe6089c674
tuannl1987 <tuannl1987.nlt@gmail.com>
2024-03-15 17:44:03 +0700

    update

---
+++ components/Navbar.tsx
@@ -0,0 +1,54 @@
+import { NavLinks } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import AuthProviders from './AuthProviders'
+import { getCurrentUser } from '@/lib/session'
+
+const Navbar = async () => {
+    const session = await getCurrentUser();
+
+    return (
+        <nav className="flexBetween navbar">
+            <div className="flex-1 flexStart gap-10">
+                <Link href="/">
+                    <Image 
+                        src="/logo.svg"
+                        width={115}
+                        height={43}
+                        alt="Flexibble"
+                    />
+                </Link>
+                <ul className="xl:flex hidden text-small gap-7">
+                    {NavLinks.map((link) => (
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="flexCenter gap-4">
+                {session?.user ? (
+                    <>
+                        {session?.user?.image && (
+                            <Image 
+                                src={session.user.image}
+                                width={40}
+                                height={40}
+                                className="rounded-full"
+                                alt={session.user.name}
+                            />
+                        )}
+
+                        <Link href="/create-project">Share work</Link>
+                    </>    
+                ) : (
+                    <AuthProviders />
+                )}
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
